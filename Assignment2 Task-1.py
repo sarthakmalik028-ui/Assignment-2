d950041577f0afd19a3288a6ec52dfb9d1bb5bec
sarthakmalik028-ui <sarthakmalik028@gmail.com>
2026-02-19 12:55:39 +0530

    initial commit

---
+++ Assignment2 Task-1.py
@@ -0,0 +1,7 @@
+A = int(input("Enter a Number:"))
+
+if A%2!=0:
+    print(+A, " is an odd number")
+
+else:
+    print(+A, "is an even number")
---
+++ Assignment__2 Task-2.py
@@ -0,0 +1,8 @@
+
+Total = 0
+
+for i in range(0, 51):
+    Total += i
+
+print("the sum of numbers from 1 to 50 is :" ,+Total)
+
